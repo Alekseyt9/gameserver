d42bdfdd8bf39dcc46f75452e2ebb38fc7afc4ca
Alekseyt9 <alekseyt9@gmail.com>
2024-05-31 20:36:55 +0300

    html fixed

---
+++ cmd/server/static/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.game-tile').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'flex';
+    document.body.classList.add('modal-open');
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+    document.body.classList.remove('modal-open');
+});
